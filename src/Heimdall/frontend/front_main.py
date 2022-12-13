b62fc7ffe6f047a2b9af314483e8eb512b9ea2d7
Juho B <jopbruun@gmail.com>
2022-12-13 16:57:44 +0200

    added buttons, regex, pathlib

--- src/Heimdall/frontend/front_main.py
+++ src/Heimdall/frontend/front_main.py
@@ -1,6 +1,8 @@
+from ui_file_func import *
 from PyQt5.QtWidgets import QApplication
 from sys import exit
-from ui_file_func import *
+from webbrowser import open as web_open
+
 
 if __name__ == "__main__":
     app = QApplication([])
@@ -8,9 +10,12 @@ if __name__ == "__main__":
     window.config = get_config(CONFIG_PATH)
     winThread = WindowUpdateThread(window)
     winThread.start()
-    window.submitButton.clicked.connect(lambda: window.getConfigValues())
+    window.submitButton.clicked.connect(window.getConfigValues)
+    window.dashboardButton.clicked.connect(lambda: web_open(DASHBOARD_URL))
+    window.scanButton.clicked.connect(lambda: call_process(['sh', str(PATH_TO_START_SCRIPT)]))
     try:
-        with open("./style.qss", "r") as file:
+        
+        with open(str(STYLEPATH), "r") as file:
             _style = file.read()
             app.setStyleSheet(_style)
     except:
