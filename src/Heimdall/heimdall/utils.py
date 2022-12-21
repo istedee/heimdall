72a2cd317bb0161a775fef96fabe4e9ad8feeebe
Mikkeep <mikke.pennanen@gmail.com>
2022-12-21 14:30:47 +0200

    Made parser for metadata of parsed JSON objects. Saves file to a file in results. Used for inspection from frontend

--- src/Heimdall/heimdall/utils.py
+++ src/Heimdall/heimdall/utils.py
@@ -1,6 +1,12 @@
 """Utility methods for Heimdall"""
 
 from pathlib import Path
+import csv
+
+from Heimdall.heimdall.logger_config import Logger
+
+
+logger = Logger(__name__)
 
 
 def return_path(relpath: str) -> str:
@@ -26,6 +32,60 @@ def parse_results(results: dict, timestamp) -> list:
             elkJson["banner"] = data.get("script", {}).get("banner", "")
             elkList.append(elkJson)
     return elkList
+
+
+def save_metadata(results: dict, config: dict, timestamp: str) -> None:
+    """Saves metadata to a file for diplaying in frontend"""
+    resultspath = return_path("../results/results_meta.txt")
+    data = {}
+    vulns = []
+    devices = []
+    ports = []
+    start = str(config["ports"]["start"])
+    end = str(config["ports"]["end"])
+    range = f"{start} - {end}"
+    for entry in results:
+        devices.append(entry["ip"])
+        if entry["CVE"]:
+            vulns.append(entry["CVE"])
+        ports.append(entry["port"])
+    devices_uniq = len(set(devices))
+    vulns_uniq = len(set(vulns))
+    devices_uniq = len(set(devices))
+    devices_uniq = len(set(devices))
+    try:
+        with open(resultspath, "r") as resultfile:
+            # Check if file exists
+            pass
+        with open(resultspath, "a") as resultfile:
+            fields = ["devices", "portsopen", "vulnamount", "timestamp", "rangeports"]
+            writer = csv.DictWriter(resultfile, fieldnames=fields)
+            writer.writerow(
+                {
+                    "devices": devices_uniq,
+                    "portsopen": len(ports),
+                    "vulnamount": vulns_uniq,
+                    "timestamp": timestamp,
+                    "rangeports": range,
+                }
+            )
+    except FileNotFoundError:
+
+        logger.info("Created new meta file for results")
+
+        with open(resultspath, "w") as result:
+            fields = ["devices", "portsopen", "vulnamount", "timestamp", "rangeports"]
+            writer = csv.DictWriter(result, fieldnames=fields)
+            writer.writeheader()
+            writer.writerow(
+                {
+                    "devices": devices_uniq,
+                    "portsopen": len(ports),
+                    "vulnamount": vulns_uniq,
+                    "timestamp": timestamp,
+                    "rangeports": range,
+                }
+            )
 
 
 class ContentCallback:
